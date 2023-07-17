72cbc021736d6343076e61f4511cf328dad7ea44
jeongheekim <8055kjh@gmail.com>
2023-07-17 23:51:18 +0900

    init commit

---
+++ src/main/java/com/heeverse/HeeverseTicketApplication.java
@@ -0,0 +1,13 @@
+package com.heeverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeeverseTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeeverseTicketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heeverse/HeeverseTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heeverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeeverseTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
