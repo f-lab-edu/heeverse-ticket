98b1ceb57cfbb45dc8aeb08d19fc82226cd8ef79
jeongheekim <8055kjh@gmail.com>
2023-07-28 13:56:18 +0900

    refactor : 제약조건 필드명과 함께 명시

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,12 +2,10 @@ DROP TABLE IF EXISTS member;
 
 CREATE TABLE member
 (
-    member_id       bigint auto_increment NOT NULL,
+    member_id       bigint auto_increment NOT NULL PRIMARY KEY,
     id              varchar(50)  NOT NULL,
     password        varchar(60)  NOT NULL,
     email           varchar(100) NOT NULL,
     user_name       varchar(20)  NOT NULL,
     create_datetime datetime     NOT NULL
 );
-
-ALTER TABLE member ADD CONSTRAINT `PK_MEMBER` PRIMARY KEY (member_id);
