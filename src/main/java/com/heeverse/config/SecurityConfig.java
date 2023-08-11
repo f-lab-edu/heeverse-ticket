e1262fc07fa8d3caa002333ce2611777443a13d3
jeongheekim <8055kjh@gmail.com>
2023-08-11 18:13:50 +0900

    feat : h2 security 설정 추가

--- src/main/java/com/heeverse/config/SecurityConfig.java
+++ src/main/java/com/heeverse/config/SecurityConfig.java
@@ -1,33 +1,42 @@
 package com.heeverse.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.heeverse.security.JsonAuthenticationFilter;
+import com.heeverse.security.JwtAuthenticationFilter;
+import com.heeverse.security.JwtTokenProvider;
+import jakarta.servlet.DispatcherType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import security.JwtTokenProvider;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
 /**
  * @author gutenlee
  * @since 2023/07/22
  */
-@EnableWebSecurity
+@RequiredArgsConstructor
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final ObjectMapper objectMapper;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public SecurityConfig(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
 
     @Bean
     public static PasswordEncoder passwordEncoder() {
@@ -35,14 +44,33 @@ public class SecurityConfig {
     }
 
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    SecurityFilterChain h2ConsoleSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher(PathRequest.toH2Console());
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.headers((headers) -> headers.frameOptions(FrameOptionsConfig::sameOrigin));
+        return http.build();
+    }
+
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.csrf(AbstractHttpConfigurer::disable)
-            /*.addFilterAt(new JsonAuthenticationFilter(
+            .securityContext(securityContext -> {
+                securityContext.securityContextRepository(
+                    new HttpSessionSecurityContextRepository());
+            })
+            .addFilterAt(new JwtAuthenticationFilter(jwtTokenProvider),
+                BasicAuthenticationFilter.class)
+            .addFilterAt(new JsonAuthenticationFilter(
                 authenticationManager(http.getSharedObject(AuthenticationConfiguration.class)),
-                new JwtTokenProvider(), objectMapper), UsernamePasswordAuthenticationFilter.class)
-            */.authorizeHttpRequests((request) -> request
-                .requestMatchers(HttpMethod.GET, "/h2-console").permitAll()
+                jwtTokenProvider, objectMapper), UsernamePasswordAuthenticationFilter.class)
+
+            .authorizeHttpRequests((request) -> request
+
+                .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll()
+                .requestMatchers("/login").permitAll()
                 .requestMatchers(HttpMethod.POST, "/member").permitAll()
                 .requestMatchers(HttpMethod.POST, "/concert").permitAll()
                 .anyRequest().authenticated());
@@ -50,9 +78,9 @@ public class SecurityConfig {
         return http.build();
     }
 
-    /*@Bean
+    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig)
         throws Exception {
         return authConfig.getAuthenticationManager();
-    }*/
+    }
 }
