5573b2df1eef04ec32b69d493ff820d98ca91a6b
jeongheekim <8055kjh@gmail.com>
2023-08-18 00:36:35 +0900

    refactor : 상수 공통화

--- src/test/java/com/heeverse/concert/dto/ConcertRequestDtoTest.java
+++ src/test/java/com/heeverse/concert/dto/ConcertRequestDtoTest.java
@@ -9,13 +9,14 @@ import org.junit.jupiter.api.Test;
  */
 class ConcertRequestDtoTest {
 
+    //TODO 특정 유효성 검사를 통과한다로 세분화 하기
     @DisplayName("ConcertRequestDto 유효성 검사 통과한다.")
     @Test
     void requestDto_validation_success_test() {
         Long artistId = 3L;
         Long venueId = 1L;
 
-        ConcertRequestDto dto = new ConcertRequestDto();
+        //ConcertRequestDto dto = new ConcertRequestDto();
 
     }
 }
