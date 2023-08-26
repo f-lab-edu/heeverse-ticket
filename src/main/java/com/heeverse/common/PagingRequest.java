7bc2321d3a74c90be63191d203b5c72cad51105e
jeongheekim <8055kjh@gmail.com>
2023-08-26 13:46:00 +0900

    refactor : enum 명칭 변경

--- src/main/java/com/heeverse/common/PagingRequest.java
+++ src/main/java/com/heeverse/common/PagingRequest.java
@@ -6,23 +6,23 @@ package com.heeverse.common;
  * @date 2023/08/23
  */
 public class PagingRequest extends AbstractPaging {
-    private final SORTING sorting;
+    private final Sorting sorting;
 
-    public PagingRequest(int page, int size, SORTING sorting) {
+    public PagingRequest(int page, int size, Sorting Sorting) {
         super(page, size);
-        this.sorting = sorting;
+        this.sorting = Sorting;
     }
 
     public static PagingRequest of(int page, int size) {
-        return of(page, size, SORTING.ASC);
+        return of(page, size, Sorting.ASC);
     }
 
-    public static PagingRequest of(int page, int size, SORTING asc) {
+    public static PagingRequest of(int page, int size, Sorting asc) {
         return new PagingRequest(page, size, asc);
     }
 
     @Override
-    public SORTING getSorting() {
+    public Sorting getSorting() {
         return sorting;
     }
 }
