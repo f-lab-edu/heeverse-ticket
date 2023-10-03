17f1b243b17d0ac0b45d907e6bdf4225211e1b8f
gutenLee <3bnm1128@naver.com>
2023-10-03 16:40:50 +0900

    fix : script 문법 오류 수정

--- script/runNcpApi.sh
+++ script/runNcpApi.sh
@@ -20,11 +20,20 @@ function makeSignature() {
 	SIGNATURE=$(echo -n -e "$SIG"|iconv -t utf8 |openssl dgst -sha256 -hmac $SECRETKEY -binary|openssl enc -base64)
 
 
-	curl -s 'https://ncloud.apigw.ntruss.com'$URI \
+
+	responseCode=$(curl -s 'https://ncloud.apigw.ntruss.com'$URI \
 		--header "Content-type:application/json" \
 		--header "x-ncp-apigw-timestamp:$TIMESTAMP" \
 		--header "x-ncp-iam-access-key:$ACCESSKEY" \
-		--header "x-ncp-apigw-signature-v2:$SIGNATURE | sed -n 's/.*<returnCode>\(.*\)<\/returnCode>.*/\1/p'"
+		--header "x-ncp-apigw-signature-v2:$SIGNATURE" | sed -n 's/.*<returnCode>\(.*\)<\/returnCode>.*/\1/p')
+
+	if [ "$responseCode" -eq 0 ]; then
+		echo 'Successfully done.'
+	else
+		echo 'Failed.'
+		echo $responseCode
+	fi
+
 
 }
 
